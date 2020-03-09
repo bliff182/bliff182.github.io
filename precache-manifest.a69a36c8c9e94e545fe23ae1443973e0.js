a067ec1cfd769c60c434592ebb687932cb959ba6
bliff182 <michael.bither@gmail.com>
2020-03-09 17:08:19 -0400

    Updates

---
+++ precache-manifest.a69a36c8c9e94e545fe23ae1443973e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b18894bc144f30583b93",
+    "url": "/static/css/main.f80d0a12.chunk.css"
+  },
+  {
+    "revision": "b18894bc144f30583b93",
+    "url": "/static/js/main.b18894bc.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29e26160ecfd53ce53e73a4b077f81e1",
+    "url": "/index.html"
+  }
+];
